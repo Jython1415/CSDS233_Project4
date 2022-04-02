3b2cd57b9c29b3a75970003d8e0d850756af6a79
Joshua Shew <joshua.t.shew@gmail.com>
2022-04-02 15:00:49 -0400

    Tokenizer class - intial writeup

---
+++ src/Tokenizer.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.util.ListIterator;
+
+/**
+ * Reads a text file line by line to extract and normalize all words
+ * @author Joshua Shew
+ */
+public class Tokenizer {
+    /* stores the list of as provided in the array or read from the txt file */
+    private ArrayList<String> wordList;
+
+    /**
+     * Read a txt file to extract and normalize all words
+     * @param path the path of the txt file to read
+     */
+    public Tokenizer(String path) throws Exception{
+        Scanner scanner;
+        try {
+            scanner = new Scanner(new File(path));
+        }
+        catch (Exception e) {
+            throw new Exception("File could not be found");
+        }
+
+        while (scanner.hasNextLine()) {
+            for (String word : scanner.nextLine().split(" "))
+                wordList.add(normalizeWord(word));
+        }
+
+        cleanWordList();
+    }
+
+    /**
+     * Normalizes a list of words
+     * @param words
+     */
+    public Tokenizer(String[] words) {
+        for (String word : words) {
+            wordList.add(normalizeWord(word));
+        }
+
+        cleanWordList();
+    }
+
+    /**
+     * Provides the processed list of words
+     * @return the list of words
+     */
+    ArrayList<String> wordList() {
+        return this.wordList;
+    }
+
+    /**
+     * Normalizes words by converting to lower case, removing spaces, and removing punctuation
+     * @param word the word to normalize
+     * @return the normalized word
+     */
+    private String normalizeWord(String word) {
+        word.toLowerCase().replaceAll(" ", "").replaceAll("\\p{Punct}", "");
+
+        return word;
+    }
+
+    /**
+     * Cleans up the wordList by removing empty strings
+     */
+    private void cleanWordList() {
+        ListIterator iterator = this.wordList.listIterator();
+
+        while (iterator.hasNext()) {
+            if (iterator.next().equals(""))
+                iterator.remove();
+        }
+    }
+}
