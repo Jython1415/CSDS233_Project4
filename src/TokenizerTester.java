68010351ec7a849d4addd935b9701510965d7573
Joshua Shew <joshua.t.shew@gmail.com>
2022-04-04 20:38:04 -0400

    TokenizerTester outline

---
+++ src/TokenizerTester.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TokenizerTester {
+    
+    /**
+     * Unit tests for Tokenizer constructors
+     * This also tests the wordList method
+     */
+    @Test
+    public void testTokenizer() {
+
+    }
+}
